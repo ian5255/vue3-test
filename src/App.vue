70936a783e1b56a72b18d5bb2534a14d4c225b38
ian5258 <ian56897@icloud.com>
2022-10-14 08:33:13 +0800

    [modify] eslint rule

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<template lang="pug">
+#app
+  router-view
 </template>
 
 <style lang="scss">
---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+#Login
+</template>
+
+<script>
+export default {
+  name: "Login-page",
+  setup () {
+    // 123
+    console.log(111); // 123
+    const a = {
+      1: "111",
+      2: "222"
+    };
+    return {
+      a
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
